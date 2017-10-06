a0142a3e0a03d0c8667a82cf8db675f8f8de2d4b
Ralph Morelli <ram8647@gmail.com>
2017-10-06 15:35:36 -0400

    Change CHECK to SUBMIT for edX.

--- quizme-helper.js
+++ quizme-helper.js
@@ -881,14 +881,14 @@ Blockly.Quizme.giveFeedback = function(isCorrect, correctStr, mistakeStr, redo) 
 
       if (answer_tries >= MAX_TRIES) {
         if (ED_X) {
-          mistakeStr = "Looks like you're struggling on this one.<br/> Try clicking <b>CHECK</b> and the  <b>SHOW ANSWER</b> button will appear.";
+          mistakeStr = "Looks like you're struggling on this one.<br/> Try clicking <b>SUBMIT</b> and the  <b>SHOW ANSWER</b> button will appear.";
 	} else {
           mistakeStr = "Looks like you're struggling on this one.<br/> Try asking for help.";
 	}
 	answer_tries = 0;
       }
       if (ED_X) {
-        correctStr = correctStr + "<br />" + "Remember to click the <b>CHECK</b> button below.";
+        correctStr = correctStr + "<br />" + "Remember to click the <b>SUBMIT</b> button below.";
       }
     }
 
